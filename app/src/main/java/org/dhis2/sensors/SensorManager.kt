35795b0cfe171fa67006318e5388cd21c9bf638f
MCHIHANA <Misheckgchihana@gmail.com>
2026-04-13 19:54:55 +0200

    feat(sensor): add randomization to TEMPERATURE

--- app/src/main/java/org/dhis2/sensors/SensorManager.kt
+++ app/src/main/java/org/dhis2/sensors/SensorManager.kt
@@ -6,7 +6,7 @@ import kotlin.random.Random
 class SensorManager {
     fun readSensor(type: SensorType): String {
         return when (type) {
-            SensorType.TEMPERATURE -> "36.7"
+            SensorType.TEMPERATURE -> String.format("%.1f", Random.nextDouble(36.0, 38.0))
             SensorType.WEIGHT -> "65"
             SensorType.HEART_RATE -> "72"
             SensorType.BLOOD_PRESSURE -> "120/80"
