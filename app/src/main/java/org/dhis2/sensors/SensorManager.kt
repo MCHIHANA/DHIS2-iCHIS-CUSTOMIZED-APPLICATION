df79f55f4a2a5824ad586d8e17ef25b4fc1f5b80
MCHIHANA <Misheckgchihana@gmail.com>
2026-04-13 19:55:57 +0200

    feat(sensor): add randomization to WEIGHT

--- app/src/main/java/org/dhis2/sensors/SensorManager.kt
+++ app/src/main/java/org/dhis2/sensors/SensorManager.kt
@@ -7,7 +7,8 @@ class SensorManager {
     fun readSensor(type: SensorType): String {
         return when (type) {
             SensorType.TEMPERATURE -> String.format("%.1f", Random.nextDouble(36.0, 38.0))
-            SensorType.WEIGHT -> "65"
+            SensorType.WEIGHT -> Random.nextInt(50, 90).toString()
+
             SensorType.HEART_RATE -> Random.nextInt(60, 100).toString()
 
             SensorType.BLOOD_PRESSURE -> "120/80"
