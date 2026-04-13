2ad4a4492d4daea4f4cea28fd7a8e4129bc843d6
MCHIHANA <Misheckgchihana@gmail.com>
2026-04-13 19:52:24 +0200

    feat(sensor): implement hardcoded WEIGHT reading

--- app/src/main/java/org/dhis2/sensors/SensorManager.kt
+++ app/src/main/java/org/dhis2/sensors/SensorManager.kt
@@ -4,6 +4,7 @@ class SensorManager {
     fun readSensor(type: SensorType): String {
         return when (type) {
             SensorType.TEMPERATURE -> "36.7"
+            SensorType.WEIGHT -> "65"
             else -> ""
         }
     }
