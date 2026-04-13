1781e0e315df64b7c5af1f8b06b6630a9fd97881
MCHIHANA <Misheckgchihana@gmail.com>
2026-04-13 19:56:27 +0200

    feat(sensor): add documentation for SensorManager

--- app/src/main/java/org/dhis2/sensors/SensorManager.kt
+++ app/src/main/java/org/dhis2/sensors/SensorManager.kt
@@ -3,7 +3,16 @@ package org.dhis2.sensors
 import kotlin.random.Random
 
 
+/**
+ * Manager class for simulating medical sensor readings.
+ */
 class SensorManager {
+    /**
+     * Reads a simulated value for the given sensor [type].
+     *
+     * @param type The type of sensor to read.
+     * @return A string representation of the simulated reading.
+     */
     fun readSensor(type: SensorType): String {
         return when (type) {
             SensorType.TEMPERATURE -> String.format("%.1f", Random.nextDouble(36.0, 38.0))
