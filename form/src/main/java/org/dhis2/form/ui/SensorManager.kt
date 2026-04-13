fcd08438f86d43a4fe2d98fcc8464d9a22a278bc
LEXAHMBALE <lexahmbale5@gmail.com>
2026-04-13 21:35:57 +0200

    feat(form): implement weight simulation in SensorManager

--- form/src/main/java/org/dhis2/form/ui/SensorManager.kt
+++ form/src/main/java/org/dhis2/form/ui/SensorManager.kt
@@ -6,6 +6,7 @@ class SensorManager {
     fun readSensor(type: SensorType): String {
         return when (type) {
             SensorType.TEMPERATURE -> String.format("%.1f", Random.nextDouble(36.0, 39.0))
+            SensorType.WEIGHT -> String.format("%.1f", Random.nextDouble(50.0, 100.0))
             else -> ""
         }
     }
